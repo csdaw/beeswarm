38eba2fbe30aedd24bbae565da2848886cea2878
Charlotte Dawson <csdaw@outlook.com>
2020-07-18 17:53:02 +0100

    Implement other beeswarm methods

--- R/stat-beeswarm.R
+++ R/stat-beeswarm.R
@@ -3,19 +3,69 @@ StatBeeswarm <- ggproto("StatBeeswarm", Stat,
                           params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = TRUE,
                                                                 group_has_equal = TRUE,
                                                                 main_is_optional = TRUE)
+                          # define n.groups 
+                          params$n.groups <- length(unique(data$group))
+                          
+                          # get range of data and extend a little
+                          if (params$flipped_aes) {
+                            params$dlim <- grDevices::extendrange(data$x, f = 0.01)
+                          } else {
+                            # define dlim
+                            params$dlim <- grDevices::extendrange(data$y, f = 0.01)
+                          }
                           params
                         },
                         
                         extra_params = c("na.rm", "orientation"),
                         
-                        compute_group = function(data, scales, flipped_aes = FALSE,
-                                                 spacing = 1, side = 0L, priority = "ascending",
+                        compute_group = function(data, scales, flipped_aes = FALSE, n.groups, dlim,
+                                                 method = "swarm",
+                                                 spacing = 1, breaks = NULL, side = 0L, priority = "ascending",
                                                  corral = "none", corral.width = 0.889) {
                           
                           data <- flip_data(data, flipped_aes)
                           
-                          x.offset <- swarmx(x = rep(0, length(data$y)), y = data$y,
-                                             cex = spacing, side = side, priority = priority)$x
+                          if (method == "swarm") {
+                            x.offset <- swarmx(x = rep(0, length(data$y)), y = data$y,
+                                               cex = spacing, side = side, priority = priority)$x
+                          } else {
+                            ## non-swarm methods
+
+                            # define size.multiplier
+                            # sizeMultiplier <- par('cex') * cex * spacing
+                            size.multiplier <- spacing
+                            
+                            # define size.d an size.g
+                            size.d <- yinch(0.08, warn.log = FALSE) * size.multiplier
+                            size.g <- xinch(0.08, warn.log = FALSE) * size.multiplier
+                            
+                            # hex method specific step
+                            if (method == "hex") size.d <- size.d * sqrt(3) / 2
+                            
+                            ## first determine positions along the y axis
+                            if(is.null(breaks))
+                              breaks <- seq(dlim[1], dlim[2] + size.d, by = size.d)
+
+                            if(length(breaks) == 1 && is.na(breaks[1])) {
+                              d.index <- data$y
+                              d.pos <- data$y
+                            } else {
+                              mids <- (head(breaks, -1) + tail(breaks, -1)) / 2
+
+                              d.index <- sapply(data$y, cut, breaks = breaks, labels = FALSE)
+
+                              d.pos <- sapply(d.index, function(a) mids[a])  
+                              data$y <- d.pos
+                            }
+                            
+                            
+                            ## now determine positions along the x axis
+                            x.index <- determine_pos(d.index, method, side)
+                            
+                            x.offset <- x.index * size.g
+                          }
+                          
+                          
 
                           if (corral != "none") {
                             corral.low <- (side - 1) * corral.width / 2
@@ -64,12 +114,15 @@ StatBeeswarm <- ggproto("StatBeeswarm", Stat,
 
 stat_beeswarm <- function(mapping = NULL, data = NULL,
                           position = "identity", ...,
-                          spacing = 1, side = 0L,
+                          method = "swarm",
+                          spacing = 1, breaks = NULL, side = 0L,
                           priority = "ascending",
                           corral = "none",
                           corral.width = 0.889,
                           na.rm = FALSE, orientation = NA, show.legend = NA, 
                           inherit.aes = TRUE) {
+  stopifnot(method %in% c("swarm", "centre", "center", "hex", "square"))
+  if (method == "center") method <- "centre"
   stopifnot(priority %in% c("ascending", "descending", "density", "random", "none"))
   stopifnot(corral %in% c("none", "gutter", "wrap", "random", "omit"))
   stopifnot(is.numeric(corral.width))
@@ -83,7 +136,9 @@ stat_beeswarm <- function(mapping = NULL, data = NULL,
     params = list(
       na.rm = na.rm,
       orientation = orientation,
+      method = method,
       spacing = spacing,
+      breaks = breaks,
       side = side,
       priority = priority,
       corral = corral,
@@ -92,3 +147,33 @@ stat_beeswarm <- function(mapping = NULL, data = NULL,
     )
   )
 }
+
+determine_pos <- function(v, method, side) {
+  if(length(na.omit(v)) == 0) 
+    return(v)
+
+  v.s <- lapply(split(v, v), seq_along)
+
+  if(method %in% c("centre", "square") && side == -1)
+    v.s <- lapply(v.s, function(a) a - max(a))
+  else if(method %in% c("centre", "square") && side == 1)
+    v.s <- lapply(v.s, function(a) a - 1)
+  else if(method == "centre")
+    v.s <- lapply(v.s, function(a) a - mean(a))
+  else if(method == "square")
+    v.s <- lapply(v.s, function(a) a - floor(mean(a)))
+  else if(method == "hex") {
+    odd.row <- (as.numeric(names(v.s)) %% 2) == 1
+    if(side == 0) {
+      v.s[ odd.row] <- lapply(v.s[ odd.row], function(a) a - floor(mean(a)) - 0.25)
+      v.s[!odd.row] <- lapply(v.s[!odd.row], function(a) a - ceiling(mean(a)) + 0.25)
+    } else if(side == -1) {
+      v.s[ odd.row] <- lapply(v.s[ odd.row], function(a) a - max(a))
+      v.s[!odd.row] <- lapply(v.s[!odd.row], function(a) a - max(a) - 0.5)
+    } else if(side ==  1) {
+      v.s[ odd.row] <- lapply(v.s[ odd.row], function(a) a - 1)
+      v.s[!odd.row] <- lapply(v.s[!odd.row], function(a) a - 0.5)
+    }
+  }
+  unsplit(v.s, v)
+} 
--- test.R
+++ test.R
@@ -7,7 +7,7 @@ distro <- list(runif = runif(100, min = -3, max = 3),
 library(beeswarm)
 beeswarm::beeswarm(distro, 
                    col = 2:3, pch = 16,
-                   method = "swarm", 
+                   method = "center",
                    main = "title")
 
 
@@ -23,9 +23,16 @@ distro3 <- as.data.frame(rev(distro)) %>%
   tidyr::pivot_longer(everything(),values_to = "value", names_to = "variable", )
 
 set.seed(123)
-ggplot2::ggplot(distro3, aes(x = variable, y = value)) + 
-  stat_beeswarm() + 
+ggplot2::ggplot(distro3, aes(x = rev(variable), y = value)) + 
+  stat_beeswarm(method = "centre") + 
   scale_y_continuous(limits = c(-3, 3)) + 
+  theme(
+    plot.margin = unit(c(0.11, 0.07, 0.11, 0.07), "npc")
+  )
+
+ggplot2::ggplot(distro3, aes(y = variable, x = value)) + 
+  stat_beeswarm(corral.width = 1) + 
+  scale_x_continuous(limits = c(-3, 3)) + 
   theme(
     plot.margin = unit(c(0.12, 0.07, 0.12, 0.07), "npc")
   )
