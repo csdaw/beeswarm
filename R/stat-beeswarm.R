b4317c64921adc2d64895191320a68749a6888d5
Charlotte Dawson <csdaw@outlook.com>
2020-07-18 14:48:32 +0100

    Make StatBeeswarm work with multiple orientations

--- R/stat-beeswarm.R
+++ R/stat-beeswarm.R
@@ -1,11 +1,23 @@
 StatBeeswarm <- ggproto("StatBeeswarm", Stat,
+                        setup_params = function(data, params) {
+                          params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = TRUE,
+                                                                group_has_equal = TRUE,
+                                                                main_is_optional = TRUE)
+                          params
+                        },
                         
-                        compute_group = function(data, scales, 
+                        extra_params = c("na.rm", "orientation"),
+                        
+                        compute_group = function(data, scales, flipped_aes = FALSE,
                                                  spacing = 1, side = 0L, priority = "ascending") {
+                          
+                          data <- flip_data(data, flipped_aes)
+                          
                           x.offset <- swarmx(x = rep(0, length(data$y)), y = data$y,
                                              cex = spacing, side = side, priority = priority)$x
                           data$x <- data$x + x.offset
-                          data
+                          
+                          flip_data(data, flipped_aes)
                         },
                         
                         required_aes = c("x", "y")
@@ -13,16 +25,16 @@ StatBeeswarm <- ggproto("StatBeeswarm", Stat,
 
 stat_beeswarm <- function(mapping = NULL, data = NULL,
                           position = "identity", ...,
-                          spacing = 1,
-                          side = 0L,
+                          spacing = 1, side = 0L,
                           priority = c("ascending", "descending", "density", "random", "none"),
-                          na.rm = FALSE, show.legend = NA, 
+                          na.rm = FALSE, orientation = NA, show.legend = NA, 
                           inherit.aes = TRUE) {
   layer(
     stat = StatBeeswarm, data = data, mapping = mapping, geom = "point", 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(
       na.rm = na.rm,
+      orientation = orientation,
       spacing = spacing,
       side = side,
       priority = priority,
